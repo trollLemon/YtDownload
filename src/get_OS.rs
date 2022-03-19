a6ea45d64d557717d19df2397806a09d103e3d15
trollLemon <90001607+trollLemon@users.noreply.github.com>
2022-03-18 20:08:12 -0700

    Delete get_OS.rs
    moved this to the app.rs file

--- src/get_OS.rs
+++
@@ -1,9 +0,0 @@
-pub mod get_OS {
-    pub fn determine_default_path() -> &'static str {
-        if cfg!(windows) {
-            "C:\\Users\\*user name*\\Downloads"
-        } else {
-            "/home/*user name*/Downloads/"
-        }
-    }
-}
